242e49d2c9fb1099564d6b8d8a7d8063477224b0
subham348 <subhammahato348@gmail.com>
2024-01-18 00:15:47 +0530

    Completed the basic structure of the Recipes components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a text',
+      'https://www.indianhealthyrecipes.com/wp-content/uploads/2023/08/chole-recipe.jpg'
+    ),
+  ];
 }
